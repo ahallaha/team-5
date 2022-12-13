837ca3480a9694b7c310a61c959c188f0598c7ca
Austin Hall <hireaustinhall@gmail.com>
2022-12-13 15:56:50 -0500

    remove node_modules

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TabBar from './components/TabBar.vue'
+</script>
+
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="tab-bar"><TabBar /></div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +17,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.tab-bar {
+  width:100%;
+  position: fixed;
+  height: 8%;
+  bottom: 0;
+  left: 0;
 }
 </style>
---
+++ src/components/TabBar.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+    <div class="tab-bar">
+        <button>Home</button>
+        <button>Explore</button>
+        <button>Get Help</button>
+    </div>
+</template>
+
+<style scoped>
+    .tab-bar {
+        background-color: lightgray;
+        border-radius: 16px 16px 0px 0px;
+    }
+
+    button {
+        width: 33%;
+        background: none;
+        color: black;
+        border: none;
+        height: 100%;
+    }
+</style>
